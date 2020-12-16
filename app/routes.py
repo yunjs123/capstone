9ee3ff705d3276aac16e22c38e0ee6f324f99109
Jay <jason.strahler@gmail.com>
2020-12-16 18:19:49 -0500

    updated prediction data

--- app/routes.py
+++ app/routes.py
@@ -11,15 +11,18 @@ import io
 import shutil
 import json
 from datetime import datetime
+from datetime import timedelta
 
 email = "jstrahl1@uncc.edu"
 key = "bluecat44"
 daily_url = "https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/today/HourlyData_"
+yest_url = "https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/yesterday/HourlyData_"
 
 risks = ["No health risks today", "If you have sensative lungs you might not want to stay outside very long today, maybe an hour at most", "No outdoor activities, children should not be out", 
 "Only go outside if you really have to or atleast limit your time", "Do not go outside if you can"]
 
 daily_data = []
+yest_data = []
 
 loca = []
 
@@ -49,6 +52,7 @@ def favicon():
 def map():
     if request.method == 'POST' or request.args.get("location") is not None:
         data_collected = []
+        yest_collected = []
         print("POST REQUEST")
         if request.method == 'POST':
             state = request.form.get('search')
@@ -59,19 +63,65 @@ def map():
         search = convert_string(state)
         print(search)
         for i, n in enumerate(daily_data):
-            if (search in n[3].lower() or search in n[8].lower()) and (n[5] not in data_collected) :
+            if (search in n[3].lower() or search in n[8].lower()) and (n[5] not in data_collected):
                 data_collected.append(n[5])
                 data_collected.append(n[6])
                 data_collected.append(n[7])
                 data_collected.append(compare_values(n[7]))
                 if data_collected[0] != n[8]:
                     data_collected.insert(0, n[8])
-                
+
+        for i, n in enumerate(yest_data):
+            if (search in n[3].lower() or search in n[8].lower()) and (n[5] not in yest_collected):
+                yest_collected.append(n[5])
+                yest_collected.append(n[6])
+                yest_collected.append(n[7])
+                yest_collected.append(compare_values(n[7]))
+                if yest_collected[0] != n[8]:
+                    yest_collected.insert(0, n[8])
+
+        prediction = []
+        temp = {}
+        temp2 = {}
+        print(yest_collected)
+        for n, i in enumerate(yest_collected):
+            if i == "OZONE":
+                temp['OZONE'] = yest_collected[n+2]
+            elif i =="PM2.5":
+                temp['PM2.5'] = yest_collected[n+2]
+            elif i == "PM10":
+                temp['PM10'] = yest_collected[n+2]
+
+        for n, i in enumerate(data_collected):
+            if i == "OZONE":
+                temp2['OZONE'] = data_collected[n+2]
+            elif i =="PM2.5":
+                temp2['PM2.5'] = data_collected[n+2]
+            elif i == "PM10":
+                temp2['PM10'] = data_collected[n+2]
+
+
+        if ("OZONE" in temp):
+            prediction.append("OZONE")
+            prediction.append("PPB")
+            prediction.append((float(temp.get("OZONE")) + float(temp2.get("OZONE")))/2)
+            prediction.append(compare_values((float(temp.get("OZONE")) + float(temp2.get("OZONE")))/2))
+        if ("PM2.5" in temp):
+            prediction.append("PM2.5")
+            prediction.append("UG/M3")
+            prediction.append((float(temp.get("PM2.5"))+float(temp2.get("PM2.5")))/2)
+            prediction.append(compare_values(float(temp.get("PM2.5"))+float(temp2.get("PM2.5")))/2)
+        if ("PM10" in temp):
+            prediction.append("PM10")
+            prediction.append("UG/M3")
+            prediction.append((float(temp.get("PM10"))+float(temp2.get("PM10")))/2)
+            prediction.append(compare_values(float(temp.get("PM10"))+float(temp2.get("PM10")))/2)
+
         print(data_collected)
         
         data_collected.append(daily_data[4448][0])
         #4454
-        return render_template("chart.html", data=data_collected, risk_list=risks)
+        return render_template("chart.html", data=data_collected, risk_list=risks, prediction=prediction)
     return render_template("chart.html", data='')
 
 @app.route('/places')
@@ -163,4 +213,19 @@ if os.path.exists(f"app/files/2020{datetime.now().strftime('%m')}{datetime.now()
             for line in Lines:
                 daily_data.append(line.split("|"))
         load_loca()
-        
+prev_day = datetime.today() - timedelta(days=1)
+prev_day_form = prev_day.strftime ('%Y%m%d')
+
+# https://s3-us-west-1.amazonaws.com//files.airnowtech.org/airnow/yesterday/HourlyData_2020121500.dat
+if not os.path.exists(f"app/files/{str(prev_day_form)}00.dat"):
+    print(yest_url + f"{str(prev_day_form)}00.dat")
+    wget.download(yest_url + f"{str(prev_day_form)}00.dat", out=f"app/files/{str(prev_day_form)}00.dat")
+    with open(f"app/files/{str(prev_day_form)}00.dat" , encoding='utf-8') as file2:
+            Lines = file2.readlines()
+            for line in Lines:
+                yest_data.append(line.split("|"))
+else:
+    with open(f"app/files/{str(prev_day_form)}00.dat" , encoding='utf-8') as file2:
+            Lines = file2.readlines()
+            for line in Lines:
+                yest_data.append(line.split("|"))
